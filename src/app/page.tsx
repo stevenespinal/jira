f5f11246e3cc8b682593d62e39c1548c34285068
Steven Espinal <stevenjesusespinal@gmail.com>
2024-10-09 17:30:03 -0400

    add: sessionMiddleware in auth routes and extend context

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,25 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { useCurrent } from "@/features/auth/api/use-current";
+import { useLogout } from "@/features/auth/api/use-logout";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 export default function Home() {
-  return <div>Home Page</div>;
+  const { data, isLoading } = useCurrent();
+  const { mutate } = useLogout();
+  const router = useRouter();
+  useEffect(() => {
+    if (!data && !isLoading) {
+      router.push("/sign-in");
+    }
+  }, [data]);
+  console.log("🚀 ~ Home ~ data:", data);
+
+  return (
+    <div>
+      Home Page: Only visible to authorized users
+      <Button onClick={() => mutate()}>Logout</Button>
+    </div>
+  );
 }
