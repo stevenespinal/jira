fbc32085849bed09c6116b35d530147249fd9189
Steven Espinal <stevenjesusespinal@gmail.com>
2024-10-10 16:40:40 -0400

    add: get session using next/headers cookies to protect routes

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,7 +1,11 @@
-"use client";
+import { getCurrent } from "@/features/auth/actions";
 import { SignUpCard } from "@/features/auth/components/sign-up-card";
+import { redirect } from "next/navigation";
 
-const SignUp = () => {
+const SignUp = async () => {
+  const user = await getCurrent();
+  if (user) redirect("/");
+
   return <SignUpCard />;
 };
 
